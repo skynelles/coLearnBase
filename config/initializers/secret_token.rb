cf71abe885ced1000c9d670b2b832f5da4f2c2e1
Joel Bouwkamp <siftd106@yahoo.com>
2013-05-14 19:18:51 -0400

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+CoLearnBase::Application.config.secret_key_base = '3d582c94ca4e19d91a39fc63d02f3ba898e45a42b7caf96ec137c655f797a43160e38e541783d3942666f0ee6d9c4bb36fa673b1f071cfb956f5c98a3fa5f0a9'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+CoLearnBase::Application.config.session_store :cookie_store, key: '_co_learn_base_session'
