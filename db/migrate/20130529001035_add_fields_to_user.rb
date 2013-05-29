3d4ee04c2632bf8e48baef2f2e257edb32c21d1d
Kurt Hansen <khansen@highfxmedia.com>
2013-05-28 20:23:34 -0400

    Add columns to user table

---
+++ db/migrate/20130529001035_add_fields_to_user.rb
@@ -0,0 +1,9 @@
+class AddFieldsToUser < ActiveRecord::Migration
+  def change
+  	add_column :users, :first_name, :string
+		add_column :users, :last_name, :string
+		add_column :users, :phone, :string
+		add_column :users, :company_name, :string
+		add_column :users, :user_type_id, :integer
+  end
+end
