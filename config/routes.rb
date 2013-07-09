b8fffbe813d72f4df89e65989481a9ab879f3dcc
Kurt Hansen <khansen@highfxmedia.com>
2013-07-09 18:42:13 -0400

    Generate controller for students and add index action.

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Stalker::Application.routes.draw do
   devise_for :users
 
+  resources :students, :only => [:index]
+
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
---
+++ spec/controllers/students_controller_spec.rb
@@ -0,0 +1,6 @@
+require 'spec_helper'
+
+describe StudentsController do
+	it {should respond_to :index}
+
+end
